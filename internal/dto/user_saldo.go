58dfa9af5b0df19a14baa2cfdede558d39773e10
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-02-18 09:00:05 +0700

    Add current balance response struct

--- internal/dto/user_saldo.go
+++ internal/dto/user_saldo.go
@@ -4,3 +4,6 @@ type TransactionBody struct {
 	NoRekening string `json:"no_rekening" binding:"required"`
 	Nominal    string `json:"nominal" binding:"required"`
 }
+type CurrentBalanceResponse struct {
+	SaldoSaatIni string `json:"saldo_saat_ini" binding:"required"`
+}
