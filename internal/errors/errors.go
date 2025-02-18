d49aeec1aa00bd71e5a155d4da228bcce1d9def2
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-02-18 09:01:04 +0700

    add Error12DigitsAndOnlyNumber, NumberWithoutThousandSeparator,MustValidNumericValue

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -10,6 +10,10 @@ var (
 	ErrorNoHPAlreadyUsed = errors.New("No Hp already used")
 	ErrorNIKAlreadyUsed  = errors.New("NIK already used")
 
+	Error12DigitsAndOnlyNumber     = errors.New("invalid input: must be exactly 12 digits and contain only numbers")
+	NumberWithoutThousandSeparator = errors.New("invalid input: must contain only numbers without thousand separators (e.g., 1000, 25000)")
+	MustValidNumericValue          = errors.New("invalid input: must be a valid numeric value")
+
 	InvalidNameLength        = errors.New("name must be between 2 and 50 characters long")
 	InvalidNameFormat        = errors.New("name can only contain letters and spaces, and must start and end with a letter")
 	InvalidPhoneNumberFormat = errors.New("invalid phone number format: must contain only numbers and may start with '+'")
