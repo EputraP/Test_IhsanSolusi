475f498f931850f6028befa6f4b0f2c243921610
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-02-18 12:54:34 +0700

    rename file

---
+++ internal/repository/user_saldo_repository.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"time"
+
+	"github.com/EputraP/Test_IhsanSolusi/internal/model"
+	"gorm.io/gorm"
+)
+
+type UserSaldoRepository interface {
+	CreateUserSaldo(inputModel *model.UserSaldo) (*model.UserSaldo, error)
+}
+
+type userSaldoRepository struct {
+	db *gorm.DB
+}
+
+func NewUserSaldoRepository(db *gorm.DB) UserSaldoRepository {
+	return &userSaldoRepository{
+		db: db,
+	}
+}
+
+func (r *userSaldoRepository) CreateUserSaldo(inputModel *model.UserSaldo) (*model.UserSaldo, error) {
+
+	sqlScript := `INSERT INTO user_saldo (no_rekening , saldo , created_at) 
+				VALUES (?,?,?) 
+				RETURNING id;`
+
+	res := r.db.Raw(sqlScript, inputModel.NoRekening, inputModel.Saldo, time.Now()).Scan(&inputModel)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return inputModel, nil
+}
+
+func (r *userSaldoRepository) UpdateUserSaldo(inputModel *model.User) (*model.User, error) {
+
+	sqlScript := `INSERT INTO users (no_rekening , nama , nik, no_hp, created_at) 
+				VALUES (?,?,?,?,?) 
+				RETURNING no_rekening;`
+
+	res := r.db.Raw(sqlScript, inputModel.NoRekening, inputModel.Nama, inputModel.NIK, inputModel.NoHP, time.Now()).Scan(&inputModel)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return inputModel, nil
+}
